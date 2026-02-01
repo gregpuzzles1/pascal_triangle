6b45dcc7768a7d9aa7068720d81ae559158dbd42
Greg Christian <gregpuzzles1@gmail.com>
2026-02-01 14:47:44 -0700

    Update footer links

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,5 +1,5 @@
 export const LINKS = {
   license: "https://opensource.org/licenses/MIT",
-  repo: "https://github.com/GREGP/pascal_triangle",
-  issues: "https://github.com/GREGP/pascal_triangle/issues/new",
+  repo: "https://github.com/gregpuzzles1/pascal_triangle",
+  issues: "https://github.com/gregpuzzles1/pascal_triangle/issues/new",
 };
