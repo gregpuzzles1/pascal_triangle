89d831167ac860193a7bbbd94dc3e0640b7b56de
Greg Christian <gregpuzzles1@gmail.com>
2026-02-01 14:52:42 -0700

    Update footer link and layout

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,5 +1,5 @@
 export const LINKS = {
-  license: "https://opensource.org/licenses/MIT",
+  license: "https://github.com/gregpuzzles1/pascal_triangle/blob/main/LICENSE",
   repo: "https://github.com/gregpuzzles1/pascal_triangle",
   issues: "https://github.com/gregpuzzles1/pascal_triangle/issues/new",
 };
